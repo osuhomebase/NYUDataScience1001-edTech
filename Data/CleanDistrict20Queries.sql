08a5929cfe0f2aa79c8294277cbd0a6c8a23f7a2
osuhomebase <mbaron999@gmail.com>
2016-11-13 16:50:09 -0500

    Added 3 year calculations

--- Data/CleanDistrict20Queries.sql
+++ Data/CleanDistrict20Queries.sql
@@ -3,10 +3,9 @@ update nycDistrict20 SET SchoolYear = left(SchoolYear,4)
 
 
 --- Extract previous year ---
-UPDATE d1 SET d1.previousYear = d2.count FROM nycDistrict20 d1
+UPDATE d1 SET d1.previousYear = d2.[count] FROM nycDistrict20 d1
 	INNER JOIN nycDistrict20 d2 on d1.censusTract = d2.CensusTract AND d1.GradeLevel = d2.GradeLevel
 	AND d2.SchoolYear = (d1.SchoolYear - 1)
-
 
 -- Anytime the previous Year is null, set to zero for any grade level greater than Kindergarten --
 UPDATE nycDistrict20 SET previousYear = 0 WHERE previousYear IS NULL AND SchoolYear > 2001
@@ -20,11 +19,34 @@ UPDATE nycDistrict20 SET OneYearPercentGrowth = (convert(numeric(9,6),[Count])-P
 -- Figure out the YoY Actual Growth
 UPDATE nycDistrict20 SET OneYearGrowth = [Count] - [PreviousYear] 
 
+-- Set Year over 
+
+
+
+--- Extract previous **three** year ---
+UPDATE d1 SET d1.previousThreeYear = d2.[count] FROM nycDistrict20 d1
+	INNER JOIN nycDistrict20 d2 on d1.censusTract = d2.CensusTract AND d1.GradeLevel = d2.GradeLevel
+	AND d2.SchoolYear = (d1.SchoolYear - 3)
+
+-- Anytime the previous **three** Year is null, set to zero for any grade level greater than Kindergarten --
+UPDATE nycDistrict20 SET previousThreeYear = 0 WHERE previousThreeYear IS NULL AND SchoolYear > 2003
+
+-- Set Thought is there is this may be more accurate than assuming zero. --
+UPDATE nycDistrict20 SET previousThreeYear = [Count] WHERE SchoolYear IN(2001,2002,2003)
+
+-- Figure out the Year over **Three** Year Percentage Growth
+UPDATE nycDistrict20 SET ThreeYearPercentGrowth = (convert(numeric(9,6),[Count])-PreviousThreeYear)/PreviousThreeYear WHERE PreviousThreeYear > 0
+
+-- Figure out the YoY Actual Growth
+UPDATE nycDistrict20 SET ThreeYearGrowth = [Count] - PreviousThreeYear 
+
+
+
 
 -- Just to verify results --
 SELECT * FROM nycDistrict20 d1
 	INNER JOIN nycDistrict20 d2 on d1.censusTract = d2.CensusTract AND d1.GradeLevel = d2.GradeLevel
-	AND d2.SchoolYear = (d1.SchoolYear - 1)
+	AND d2.SchoolYear = (d1.SchoolYear - 3)
 	AND d1.GradeLevel=3
 ORDER BY d1.CensusTract, d1.SchoolYear, d1.GradeLevel
 
