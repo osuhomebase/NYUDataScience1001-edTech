9a375bb9afdc5d3907807839677d9453dac146f0
osuhomebase <mbaron999@gmail.com>
2016-11-30 20:47:43 -0500

    added previous, growth, 3 year avg to update statement.  Limited by specific fields because query was taking forever.

--- Data/Create311CountTable.sql
+++ Data/Create311CountTable.sql
@@ -4,7 +4,13 @@ DECLARE @sql varchar(max)
 DECLARE @type varchar(50) 
 DECLARE @first tinyint
 
-DECLARE curs CURSOR FOR SELECT distinct Replace(Replace(Replace(Replace(Replace(Replace([Complaint Type],' ',''),'/',''),'-',''),'(',''),')',''),'&','')  from nyc311requests
+-- Grab unique request types from 311 Requests... remove the obvious features that aren't good predictors --
+DECLARE curs CURSOR FOR SELECT distinct Replace(Replace(Replace(Replace(Replace(Replace([Complaint Type],' ',''),'/',''),'-',''),'(',''),')',''),'&','')  from nyc311requests 
+	WHERE Replace(Replace(Replace(Replace(Replace(Replace([Complaint Type],' ',''),'/',''),'-',''),'(',''),')',''),'&','')  NOT IN
+	('Squeegee','RadioactiveMaterial','Snow','Lifeguard','BikeRollerSkateChronic','InvestigationsandDisciplineIAD','NONCONST','IllegalFireworks','HarboringBeesWasps','FoodPoisoning',
+		'DisciplineandSuspension','CalorieLabeling','AdoptABasket','AnnualCycleInspection','APPLIANCE','BeachPoolSaunaComplaint','BottledWater','BridgeCondition',
+		'DOFPropertyCityRebate','Elevator','HazardousMaterials','IllegalTreeDamage','HighwaySignDangling','BESTSiteSafety','RegistrationandTransfers','PoisonIvy','RadioactiveMaterial',
+		'STARExemption','CollectionTruckNoise','FoundProperty','HighwayCondition','HighwaySignDamaged','MiscellaneousCategories','StalledSites','TaxiComplaint','XRayMachineEquipment')
 OPEN curs
 
 SET @sql = 'CREATE TABLE nyc311counts (
@@ -15,6 +21,12 @@ FETCH NEXT FROM curs INTO @type
 WHILE @@FETCH_STATUS=0
 BEGIN
 	SET @sql = @sql + ',' + @type + ' int DEFAULT 0'
+	SET @sql = @sql + ',' + @type + 'Previous int DEFAULT 0'
+	SET @sql = @sql + ',' + @type + 'Growth int DEFAULT 0'
+	SET @sql = @sql + ',' + @type + 'PercentGrowth numeric(9,6) DEFAULT 0'
+	SET @sql = @sql + ',' + @type + 'PreviousThree int DEFAULT 0'
+	SET @sql = @sql + ',' + @type + 'GrowthThree int DEFAULT 0'
+	SET @sql = @sql + ',' + @type + 'PercentGrowthThree numeric(9,6) DEFAULT 0'
 	
 	FETCH NEXT FROM curs INTO @type
 END
