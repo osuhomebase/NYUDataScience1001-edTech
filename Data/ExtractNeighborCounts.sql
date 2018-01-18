50a3fa2b047ca8fea92f2bb856bb54d4a0b4e6d7
osuhomebase <mbaron999@gmail.com>
2018-01-18 07:17:28 -0500

    whitespace

--- Data/ExtractNeighborCounts.sql
+++ Data/ExtractNeighborCounts.sql
@@ -1,7 +1,7 @@
 select c.*, 
 IsNull(c1.[count],0) + IsNull(c2.[count],0) + IsNull(c3.[count],0) + IsNull(c4.[count],0) + IsNull(c5.[count],0) + IsNull(c6.[count],0) as neighborCount,
-IsNull(c1.ThreeYearGrowthmavg3,0) + IsNull(c2.ThreeYearGrowthmavg3,0) + IsNull(c3.ThreeYearGrowthmavg3,0) + IsNull(c4.ThreeYearGrowthmavg3,0) + IsNull(c5.ThreeYearGrowthmavg3,0) + IsNull(c6.ThreeYearGrowthmavg3,0) as neighborThreeYearGrowthmavg3,
-IsNull(c1.ThreeYearGrowth,0) + IsNull(c2.ThreeYearGrowth,0) + IsNull(c3.ThreeYearGrowth,0) + IsNull(c4.ThreeYearGrowth,0) + IsNull(c5.ThreeYearGrowth,0) + IsNull(c6.ThreeYearGrowth,0) as neighborThreeYearGrowth
+(IsNull(c1.ThreeYearGrowthmavg3,0) + IsNull(c2.ThreeYearGrowthmavg3,0) + IsNull(c3.ThreeYearGrowthmavg3,0) + IsNull(c4.ThreeYearGrowthmavg3,0) + IsNull(c5.ThreeYearGrowthmavg3,0) + IsNull(c6.ThreeYearGrowthmavg3,0))/6 as neighborThreeYearGrowthmavg3,
+(IsNull(c1.ThreeYearGrowth,0) + IsNull(c2.ThreeYearGrowth,0) + IsNull(c3.ThreeYearGrowth,0) + IsNull(c4.ThreeYearGrowth,0) + IsNull(c5.ThreeYearGrowth,0) + IsNull(c6.ThreeYearGrowth,0))/6 as neighborThreeYearGrowth
 from cleantable c inner join census_tracts_list_36 ct on c.LongTract = ct.GEOID
 left join cleantable c1 on c1.LongTract = ct.Neighbor1 and c1.GradeLevel = c.GradeLevel and c1.SchoolYear = c.SchoolYear
 left join cleantable c2 on c2.LongTract = ct.Neighbor1 and c2.GradeLevel = c.GradeLevel and c2.SchoolYear = c.SchoolYear
